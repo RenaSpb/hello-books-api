9959760d6ec313c4d8dc2e88d869cd8e1059b7b7
Renata <murzinarenata@gmail.com>
2025-05-01 09:45:02 -0700

    wave 7

--- app/models/book.py
+++ app/models/book.py
@@ -5,3 +5,22 @@ class Book(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str]
+
+    def to_dict(self):
+        book_as_dict = {}
+        book_as_dict["id"] = self.id
+        book_as_dict["title"] = self.title
+        book_as_dict["description"] = self.description
+
+        return book_as_dict
+    
+    @classmethod
+    def from_dict(cls, book_data):
+        new_book = cls(title=book_data["title"],
+                       description=book_data["description"])
+        # We could also use `Book` in place of the `cls` keyword  
+        # The following declaration is equivalent to the one above
+        # new_book = Book(title=book_data["title"],
+        #                 description=book_data["description"])
+
+        return new_book
