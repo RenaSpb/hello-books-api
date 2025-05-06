41c376c4dc3b8f5a557a5ddcc07af3330fa1d01d
Renata <murzinarenata@gmail.com>
2025-05-05 23:15:22 -0700

    wave 8

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from .db import db, migrate
 from .models import book
 from .routes.book_routes import bp as books_bp
+from .routes.author_routes import bp as authors_bp
 import os
 
 def create_app(config=None):
@@ -11,14 +12,12 @@ def create_app(config=None):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
     if config:
-        # Merge `config` into the app's configuration
-        # to override the app's default settings for testing
         app.config.update(config)
 
     db.init_app(app)
     migrate.init_app(app, db)
 
-    # Register Blueprints here
     app.register_blueprint(books_bp)
+    app.register_blueprint(authors_bp)
 
     return app
